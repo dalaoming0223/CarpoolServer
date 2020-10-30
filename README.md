2b7e1fbfb930db8b546be155bfcb7f20f5d69860
dalaoming <779507434@qq.com>
2020-10-30 22:02:44 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,11 @@
+Koa服务端编程、异步编程、面向对象编程。
+
+- Koa 与 Koa 二次开发API
+- 多 koa-router 拆分路由
+- require-directory 自动路由加载
+- nodemon修改文件自动重启
+- 异步编程，async/await
+- 异步异常链与全局异常处理
+- Sequelize ORM 管理 MySQL
+- JWT 权限控制中间件
+- Validator 与 LinValidator 验证器
---
+++ app-test.js
@@ -0,0 +1,38 @@
+const Koa = require('koa')
+
+const app = new Koa()
+
+/**
+ * 应用程序对象  中间件
+ **/
+
+// 注册
+
+
+app.use(async (ctx, next) => {
+  console.log('测试KOA中间件1')
+  const a = await next()
+  console.log(a)
+})
+
+app.use(async (ctx, next) => {
+  console.log('测试KOA中间件2')
+  // await next()
+  return 'abcdefg'
+})
+
+
+// app.use( (ctx, next) => {
+//   console.log('测试KOA中间件1')
+//   const a = next()
+//   console.log(a)
+//   a.then((res) => console.log(res))
+// })
+
+// app.use( (ctx, next) => {
+//   console.log('测试KOA中间件2')
+//   next()
+//   return '(#`O′)！！！！！！！！！！！！！'
+// })
+
+app.listen(3000)
